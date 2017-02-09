6c070d5d8f57ea235b6881adb7bc0df641eaf12b
Carl Olson <carl@Carls-MacBook-Pro.local>
2017-02-09 10:58:56 -0600

    clean up templates

--- test/controllers/page_controller_test.exs
+++ test/controllers/page_controller_test.exs
@@ -1,13 +1,13 @@
 defmodule Chat.PageControllerTest do
   use Chat.ConnCase
 
-  test "GET /", %{conn: conn} do
+  test "doesnt contain phoenix information", %{conn: conn} do
     conn = get conn, "/"
 
     text =
       html_response(conn, 200)
       |> Floki.parse()
       |> Floki.text
-    assert text =~ "Welcome to Phoenix!"
+    refute text =~ ~r/phoenix/
   end
 end

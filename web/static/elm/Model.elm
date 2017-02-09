8583a8a8d6d2f40bfbf4abbf0b1dd39ac682a3ff
Carl Olson <carl@Carls-MacBook-Pro.local>
2017-02-09 15:47:45 -0600

    add dictionary to elm model

--- web/static/elm/Model.elm
+++ web/static/elm/Model.elm
@@ -1,10 +1,31 @@
 module Model exposing (..)
 
+import Dict exposing (Dict)
 import Material
+import Maybe exposing (..)
 
 type alias Model =
     { mdl : Material.Model
+    , store : Dict String String
     }
 
 init =
-    Model Material.model
+    Model Material.model Dict.empty
+
+put : String -> String -> Model -> Model
+put key value model =
+    { model |
+      store = Dict.insert key value model.store
+    }
+
+get : String -> Model -> Maybe String
+get key model =
+    Dict.get key model.store
+
+getDefault : String -> String -> Model -> String
+getDefault key default model =
+    case get key model of
+        Just value ->
+            value
+        Nothing ->
+            default
